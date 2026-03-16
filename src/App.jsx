4e44df5020184db7ae64417c74d935d5dc822cdf
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-16 19:52:59 +0800

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// Pages
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import History from './pages/History'
+import ChangePassword from './pages/ChangePassword'
+import UpdateRecord from './pages/UpdateRecord'
+import AddRecord from './pages/AddRecord'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// Routing
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import PublicRoute from './components/PublicRoute'
+import ProtectedRoute from './components/ProtectedRoute'
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+
+        <Route
+          path='/' 
+          element={
+            <PublicRoute>
+              <Login/>
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path='/login' 
+          element={
+            <PublicRoute>
+              <Login/>
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path='/dashboard'
+          element={
+            <ProtectedRoute>
+              <Dashboard/>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/history'
+          element={
+            <ProtectedRoute>
+              <History/>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/change-password'
+          element={
+            <ProtectedRoute>
+              <ChangePassword/>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/update-record/:recordId'
+          element={
+            <ProtectedRoute>
+              <UpdateRecord/>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path='/add-record'
+          element={
+            <ProtectedRoute>
+              <AddRecord/>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </ Router>
   )
 }
 
