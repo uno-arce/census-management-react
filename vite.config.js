98f73b3f21c5d1c6abfc31fdbc5da622d397b13e
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-16 20:00:39 +0800

    Added tailwindcss and server

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  server: {
+    host: '127.0.0.1'
+  }
 })
