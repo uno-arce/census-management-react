1c84a866bf314daae92d0e27641a48fa772016a3
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-17 23:26:03 +0800

    Added new component states

--- src/stores/componentStore.js
+++ src/stores/componentStore.js
@@ -3,8 +3,8 @@ import { useShallow } from 'zustand/react/shallow'
 
 const useComponentStore = create((set) => ({
 	// Alert
-	alertMessage: false,
-	alertMessage: null,
+	isAlertOpen: false,
+	alertStatus: null,
 	alertMessage: null,
 	actionsAlert: {
 		setIsAlertOpen: (isOpen) => set({ isAlertOpen: isOpen }),
@@ -30,51 +30,76 @@ const useComponentStore = create((set) => ({
 		setCurrentFormStep: (currentFormStep) => set({ currentFormStep: currentFormStep }),
 	},
 
+	// Sidebar
+	isSidebarOpen: false,
+	actionsSidebar: {
+		setIsSidebarOpen: (isOpen) => set({ isSidebarOpen: isOpen }),
+	},
+
 	// ThemeToggle
 	isDarkMode: localStorage.getItem('theme') === 'dark',
 	actionsThemeToggle: {
-		setIsDarkMode: (mode) => set({ isDarkMode: mode }),
+		setIsDarkMode: (mode) => {
+			set({ isDarkMode: mode });
+			localStorage.setItem('theme', mode ? 'dark' : 'light');
+			if (mode) document.documentElement.classList.add('dark');
+			else document.documentElement.classList.remove('dark');
+		},
+	},
+
+	// Search
+	isSearchActive: false,
+	actionsSearch: {
+		setIsSearchActive: (isActive) => set({ isSearchActive: isActive }),
 	}
 }))
 
 export default useComponentStore
 
-
-// Custom Hooks utilizing useShallow to prevent unnecessary re-renders
-
-// Alert
-export const useAlertData = () => useComponentStore(useShallow((state) => {
-	return {
-		isAlertOpen: state.isAlertOpen,
-		alertStatus: state.alertStatus,
-		alertMessage: state.alertMessage
-	}
-}))
+// Alert Selectors
+export const useAlertData = () => useComponentStore(useShallow((state) => ({
+	isAlertOpen: state.isAlertOpen,
+	alertStatus: state.alertStatus,
+	alertMessage: state.alertMessage
+})))
 export const useAlertActions = () => useComponentStore((state) => state.actionsAlert)
 
-// Collection
-export const useCollectionData = () => useComponentStore(useShallow((state) => {
-	return {
-		collectionItem: state.collectionItem,
-		collectionSelectedGroup: state.collectionSelectedGroup,
-		collectionSelectedIndex: state.collectionSelectedIndex,
-		isCollectionOpen: state.isCollectionOpen
-	}
-}))
+// Collection Selectors
+export const useCollectionData = () => useComponentStore(useShallow((state) => ({
+	collectionItem: state.collectionItem,
+	collectionSelectedGroup: state.collectionSelectedGroup,
+	collectionSelectedIndex: state.collectionSelectedIndex,
+	isCollectionOpen: state.isCollectionOpen
+})))
 export const useCollectionActions = () => useComponentStore((state) => state.actionsCollection)
 
-// Form
-export const useFormData = () => useComponentStore(useShallow((state) => {
-	return {
-		currentFormStep: state.currentFormStep
-	}
-}))
+// Form Selectors
+export const useFormData = () => useComponentStore(useShallow((state) => ({
+	currentFormStep: state.currentFormStep
+})))
 export const useFormActions = () => useComponentStore((state) => state.actionsForm)
 
-// ThemeToggle
-export const useThemeToggleData = () => useComponentStore(useShallow((state) => {
-	return {
-		isDarkMode: state.isDarkMode
-	}
-}))
+// Menu Selectors
+export const useMenuData = () => useComponentStore(useShallow((state) => ({
+	selectedMenuCategory: state.selectedMenuCategory,
+	selectedMenuLabel: state.selectedMenuLabel
+})))
+export const useMenuActions = () => useComponentStore((state) => state.actionsMenu)
+
+// Sidebar Selectors
+export const useSidebarData = () => useComponentStore(useShallow((state) => ({
+	isSidebarOpen: state.isSidebarOpen
+})))
+export const useSidebarActions = () => useComponentStore((state) => state.actionsSidebar)
+
+// ThemeToggle Selectors
+export const useThemeToggleData = () => useComponentStore(useShallow((state) => ({
+	isDarkMode: state.isDarkMode
+})))
 export const useThemeToggleActions = () => useComponentStore((state) => state.actionsThemeToggle)
+
+// Search Selectors
+export const useSearchData = () => useComponentStore(useShallow((state) => ({
+	isSearchActive: state.isSearchActive
+})))
+export const useSearchActions = () => useComponentStore((state) => state.actionsSearch)
