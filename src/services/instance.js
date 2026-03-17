07e328966b082c12f0d39b5c90478d603f76cd7a
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-17 23:25:32 +0800

    Modified instance to intercept

--- src/services/instance.js
+++ src/services/instance.js
@@ -8,5 +8,15 @@ const instance = axios.create({
 	}
 })
 
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+        config.headers.Authorization = token
+    }
+    return config
+}, (error) => {
+    return Promise.reject(error)
+})
+
 
 export default instance
