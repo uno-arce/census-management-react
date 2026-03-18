0c3bdf1cdf4655e5824eeb70dbbae165c57b7149
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-18 19:41:02 +0800

    Modified navbar text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
     const historyIcon = <svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="currentColor"><path d="M480-120q-75 0-140.5-28.5t-114-77q-48.5-48.5-77-114T120-480q0-75 28.5-140.5t77-114q48.5-48.5 114-77T480-840q82 0 155.5 35T760-706v-94h80v240H600v-80h110q-41-56-101-88t-129-32q-117 0-198.5 81.5T200-480q0 117 81.5 198.5T480-200q105 0 183.5-68T756-440h82q-8 139-105.5 229.5T480-120Zm50-360H340v-80h110v-160h80v240Z"/></svg>
 
     return (
-        <nav className='flex items-center justify-between px-16 py-8 bg-background'>
+        <nav className='flex items-center justify-between px-16 py-6 bg-background'>
             <div className='flex flex-col'>
                 <div className='flex items-center gap-2'>
                     <h1 className='text-accent text-3xl font-display font-bold leading-none'>RBI</h1>
@@ -39,7 +39,7 @@ export default function Navbar() {
 
                 <div className='flex items-center gap-4 text-right'>
                     <div className='flex flex-col'>
-                        <span className='text-base font-semibold'>Hi, {username || 'Gloria'}</span>
+                        <span className='text-base font-medium'>Hi, {username || 'Gloria'}</span>
                         <span className='text-base-light text-sm'>Admin</span>
                     </div>
                     <Button 
