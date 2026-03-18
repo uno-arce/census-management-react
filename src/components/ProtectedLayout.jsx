0facc11cce9defd1ae314d6889887abd84e1d207
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-18 19:41:16 +0800

    Modified protected layout styles

--- src/components/ProtectedLayout.jsx
+++ src/components/ProtectedLayout.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Sidebar from './sidebar'
-import Alert from './alert'
 
 export default function ProtectedLayout() {
     return (
-        <div>
+        <div className='flex flex-col h-screen overflow-y-auto'>
             <Navbar/>
-            <main>
+            <main className='flex-1'>
                 <Outlet/>
+                <footer className='w-full border-t border-base-light/20 py-6 text-center mt-auto'>
+                    <p className='text-base-light/60 text-xs tracking-widest uppercase'>
+                        © 2021 - 2023 Barangay Sampaloc IV
+                    </p>
+                </footer>
             </main>
             <Sidebar/>
-            <Alert />
         </div>
     )
 }
