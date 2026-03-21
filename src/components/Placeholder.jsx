777f1dbc29f2f7980912e909f5962701888c49f5
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-21 19:20:11 +0800

    Modified placeholder structure

--- src/components/Placeholder.jsx
+++ src/components/Placeholder.jsx
@@ -1,31 +1,23 @@
 import React from 'react'
 
-export default function Placeholder({ isLoading, isEmpty, skeletonNumbers, structure, emptyView, children}) {
-	const skeleton = [...Array(skeletonNumbers)].map((_, index) => (
-		<div key={index} className={`${structure.skeleton} animate-pulse`}/>
-	))
+export default function Placeholder({ isLoading, isEmpty, skeletonNumbers, structure, emptyView, children }) {
+    const skeleton = [...Array(skeletonNumbers)].map((_, index) => (
+        <div key={index} className={`${structure.skeleton} animate-pulse`} />
+    ))
 
-	return (
-		<div>
-			{isLoading ? (
-				<div
-					key='skeleton'
-					className={structure.parent}>
-					{skeleton}
-				</div>
-			) : isEmpty ? (
-				emptyView ? (
-					<div
-						key='empty'
-						className='relative z-1'>
-						{emptyView}
-					</div>
-				) : null
-			) : (
-				<div>
-					{children}
-				</div>
-			)}
-		</div>
-	)
+    if (isLoading) {
+        return structure.parent === 'tbody' ? (
+            <tbody><tr><td colSpan="12" className="p-0">{skeleton}</td></tr></tbody>
+        ) : (
+            <div className={structure.parent}>{skeleton}</div>
+        )
+    }
+
+    if (isEmpty) {
+        return emptyView ? (
+            structure.parent === 'tbody' ? <tbody>{emptyView}</tbody> : <div>{emptyView}</div>
+        ) : null
+    }
+
+    return <>{children}</>
 }
