06b479285d4d52b9664c2de8730d1e77d881feec
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-22 20:42:28 +0800

    Added history states

--- src/stores/userAuthStore.js
+++ src/stores/userAuthStore.js
@@ -4,18 +4,36 @@ import { useShallow } from 'zustand/react/shallow'
 const useUserAuthStore = create((set) => ({
     username: '',
     password: '',
+    totalHistory: 0,
+    historyLogs: [],
     isAuthenticated: false,
     isFormDisabled: false,
     isUserAuthLoading: true,
+    isHistoryLoading: false,
     isUserTester: false,
+
+    historyParams: {
+        rows: 10,
+        group: 1,
+        searchColumn: 'lastName',
+        search: '',
+        sortColumn: 'updatedAt',
+        order: 'desc'
+    },
 
     actions: {
         setUsername: (username) => set({ username }),
         setPassword: (password) => set({ password }),
+        setHistoryLogs: (logs) => set({ historyLogs: logs }),
+        setTotalHistory: (total) => set({ totalHistory: total }),
         setIsAuthenticated: (auth) => set({ isAuthenticated: auth }),
         setIsFormDisabled: (disabled) => set({ isFormDisabled: disabled }),
         setIsUserAuthLoading: (loading) => set({ isUserAuthLoading: loading }),
+        setIsHistoryLoading: (loading) => set({ isHistoryLoading: loading }),
         setIsUserTester: (isTester) => set({ isUserTester: isTester }),
+        setHistoryParams: (newParams) => set((state) => ({
+            historyParams: { ...state.historyParams, ...newParams }
+        })),
         resetUserAuthState: () => set({ username: '', password: '', isFormDisabled: false })
     }
 }))
@@ -23,9 +41,13 @@ const useUserAuthStore = create((set) => ({
 export const useUserAuthData = () => useUserAuthStore(useShallow((state) => ({
     username: state.username,
     password: state.password,
+    historyLogs: state.historyLogs,
+    totalHistory: state.totalHistory,
+    historyParams: state.historyParams,
     isAuthenticated: state.isAuthenticated,
     isFormDisabled: state.isFormDisabled,
     isUserAuthLoading: state.isUserAuthLoading,
+    isHistoryLoading: state.isHistoryLoading,
     isLoginButtonDisabled: !state.username || !state.password
 })))
 
