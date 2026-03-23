9b9713947da05e5ca0fbc0b6c6d065add4621356
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2026-03-23 20:37:01 +0800

    Added new change password states

--- src/stores/userAuthStore.js
+++ src/stores/userAuthStore.js
@@ -4,10 +4,15 @@ import { useShallow } from 'zustand/react/shallow'
 const useUserAuthStore = create((set) => ({
     username: '',
     password: '',
+    oldPassword: '',
+    newPassword: '',
     totalHistory: 0,
     historyLogs: [],
+
     isAuthenticated: false,
     isFormDisabled: false,
+    isNewPasswordLengthCorrect: false,
+    isNewPasswordComplexityCorrect: false,
     isUserAuthLoading: true,
     isHistoryLoading: false,
     isUserTester: false,
@@ -24,6 +29,10 @@ const useUserAuthStore = create((set) => ({
     actions: {
         setUsername: (username) => set({ username }),
         setPassword: (password) => set({ password }),
+        setOldPassword: (oldPassword) => set({ oldPassword }),
+        setNewPassword: (newPassword) => set({ newPassword }),
+        setIsNewPasswordLengthCorrect: (isCorrect) => set({ isNewPasswordLengthCorrect: isCorrect }),
+        setIsNewPasswordComplexityCorrect: (isCorrect) => set({ isNewPasswordComplexityCorrect: isCorrect }),
         setHistoryLogs: (logs) => set({ historyLogs: logs }),
         setTotalHistory: (total) => set({ totalHistory: total }),
         setIsAuthenticated: (auth) => set({ isAuthenticated: auth }),
@@ -34,18 +43,31 @@ const useUserAuthStore = create((set) => ({
         setHistoryParams: (newParams) => set((state) => ({
             historyParams: { ...state.historyParams, ...newParams }
         })),
-        resetUserAuthState: () => set({ username: '', password: '', isFormDisabled: false })
+        resetUserAuthState: () => set({ 
+            username: '', 
+            password: '', 
+            oldPassword: '', 
+            newPassword: '', 
+            isNewPasswordLengthCorrect: false,
+            isNewPasswordComplexityCorrect: false,
+            isFormDisabled: false 
+        })
     }
 }))
 
 export const useUserAuthData = () => useUserAuthStore(useShallow((state) => ({
     username: state.username,
     password: state.password,
+    oldPassword: state.oldPassword,
+    newPassword: state.newPassword,
     historyLogs: state.historyLogs,
     totalHistory: state.totalHistory,
     historyParams: state.historyParams,
     isAuthenticated: state.isAuthenticated,
     isFormDisabled: state.isFormDisabled,
+    isNewPasswordLengthCorrect: state.isNewPasswordLengthCorrect,
+    isNewPasswordComplexityCorrect: state.isNewPasswordComplexityCorrect,
+    isUpdatePasswordDisabled: !state.oldPassword || !state.isNewPasswordLengthCorrect || !state.isNewPasswordComplexityCorrect,
     isUserAuthLoading: state.isUserAuthLoading,
     isHistoryLoading: state.isHistoryLoading,
     isLoginButtonDisabled: !state.username || !state.password
